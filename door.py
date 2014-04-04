50cab42221d0cc685978af5cd869107ac9e7081b
Sverre Bjørke <sverrejb@stud.ntnu.no>
2014-04-04 03:45:44 +0200

    modified to only update API when door state changed

--- door.py
+++ door.py
@@ -8,19 +8,23 @@ GPIO.setup(7, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 timeout = 30.0 #seconds
 api = "http://hackerspace.idi.ntnu.no/api/door"
+door_state = GPIO.input(7)
 
 def check_door():
-	door_state = GPIO.input(7)
-	#if door closed
-	if door_state == 1:
-		requests.put(api)
-		print "door closed"
-	#else door open
-	else: 
-		requests.post(api)
-		print "door open"
+	#if state is changed
+	if door_state != GPIO.input(7):
+		door_state = GPIO.input(7)
+		#if door closed
+		if door_state == 1:
+			requests.put(api)
+			print "door closed"
+		#else door open
+		else: 
+			requests.post(api)
+			print "door open"
 
 print "Reporting door status every " + str(timeout) + " seconds"
 l = task.LoopingCall(check_door)
 l.start(timeout) # start calls every n seconds
 reactor.run()
+	
