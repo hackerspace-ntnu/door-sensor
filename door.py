b26d3cdee3c3faaff902f6efd346909e770405d7
Sverre Bjørke <sverrejb@stud.ntnu.no>
2014-04-04 03:47:34 +0200

    changed polling interval

--- door.py
+++ door.py
@@ -6,7 +6,7 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
-timeout = 30.0 #seconds
+timeout = 5.0 #seconds
 api = "http://hackerspace.idi.ntnu.no/api/door"
 door_state = GPIO.input(7)
 
