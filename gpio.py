0486a0139e4d80f86c4276a854dbc13715958ce3
Mathias Ose <mathias.ose@gmail.com>
2016-09-29 13:26:39 +0200

    Non-blocking LED blinking

--- gpio.py
+++ gpio.py
@@ -1,10 +1,18 @@
+import threading
 import time
 
 import RPi.GPIO as GPIO
 
 SENSOR_PIN = 4
 LED_PINS = R_PIN, G_PIN, B_PIN = 22, 27, 17
-BLINK_PAUSE = 0.25
+BLINK_TIMEOUT = 0.25
+COLORS = OFF, RED, GREEN, BLUE, YELLOW, WHITE = (0, 0, 0), (1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), (1, 1, 1)
+
+
+def init_gpio():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(LED_PINS, GPIO.OUT)
 
 
 def check_door():
@@ -12,24 +20,45 @@ def check_door():
     return bool(status)
 
 
-# Initializes the GPIO pins on the raspberry pi
-def init_gpio():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(LED_PINS, GPIO.OUT)
-
-    blink_led(1, 1, 1, times=3)
-
-
-def hold_led(red=0, green=0, blue=0):
+def set_led(red=0, green=0, blue=0):
     GPIO.output(R_PIN, red)
     GPIO.output(G_PIN, green)
     GPIO.output(B_PIN, blue)
 
 
-def blink_led(red=0, green=0, blue=0, times=1):
-    for _ in range(times):
-        hold_led(0, 0, 0)
-        time.sleep(BLINK_PAUSE)
-        hold_led(red, green, blue)
-        time.sleep(BLINK_PAUSE)
+class LEDControl:
+    color = OFF
+
+    def __init__(self):
+        self.off()
+        self.blinking = threading.Event()
+        self.t = threading.Thread(target=self.__blink)
+        self.t.start()
+
+    def on(self):
+        set_led(*self.color)
+
+    def off(self):
+        set_led(*OFF)
+
+    def wait(self, seconds):
+        time.sleep(seconds)
+
+    def hold(self, color):
+        self.color = color
+        self.blinking.clear()
+
+    def blink(self, color):
+        self.color = color
+        self.blinking.set()
+
+    def __blink(self):
+        while True:
+            if self.blinking.is_set():
+                self.on()
+                self.wait(BLINK_TIMEOUT)
+                self.off()
+                self.wait(BLINK_TIMEOUT)
+            else:
+                self.on()
+                self.wait(BLINK_TIMEOUT)
